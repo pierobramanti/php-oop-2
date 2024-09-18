79b47d94e8e862f32b039ab63acd2fd188ea4f28
pierobramanti <piero.bramanti94@gmail.com>
2024-09-18 18:07:23 +0200

    [UPD] commented useless code and inserted istance for product

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ class Categories {
 }
 
 
-class Product {
+class Products {
     public $name;
     public $price;
     public $categorie; 
@@ -31,7 +31,7 @@ class Product {
 
 
 // Classe figlia cibo
-class FoodProduct extends Product {
+class FoodProduct extends Products {
     public $flavor;
 
     public function __construct($name, $price, Categories $categorie, $flavor) {
@@ -45,7 +45,7 @@ class FoodProduct extends Product {
 }
 
 // Classe figlia giocattoli
-class ToyProduct extends Product {
+class ToyProduct extends Products {
     public $material;
 
     public function __construct($name, $price, Categories $categorie, $material) {
@@ -60,7 +60,7 @@ class ToyProduct extends Product {
 
 
 // Classe figlia cucce
-class KennelProduct extends Product {
+class KennelProduct extends Products {
     public $size;
     public $material;
 
@@ -79,22 +79,30 @@ class KennelProduct extends Product {
 $dog = new Categories("Cane", "🐶");
 $cat = new Categories("Gatto", "🐱");
 
-var_dump($dog);
-var_dump($cat);
+
+// Creazione delle istanze dei prodotti
+$products = [
+    new FoodProduct("Crocchette", "20$", $dog, "Chicken"),
+    new ToyProduct("Pallina", "5$", $cat, "Plastic"),
+    new KennelProduct("Cuccia", "80$", $dog, "3mq", "Cotton")
+];
+
+//var_dump($dog);
+//var_dump($cat);
 
 // Stampo per testare classe figlia cibo
-$food1 = new FoodProduct("crochette", "20$", $dog, "Chicken");
-echo $food1->GetInfo(); 
+//$food1 = new FoodProduct("crochette", "20$", $dog, "Chicken");
+//echo $food1->GetInfo(); 
 
-echo "<br>";
+//echo "<br>";
 // Stampo per testare classe figlia giochi
-$toy1 = new ToyProduct("pallina", "5$", $cat, "plastic");
-echo $toy1->GetInfo(); 
+//$toy1 = new ToyProduct("pallina", "5$", $cat, "plastic");
+//echo $toy1->GetInfo(); 
 
 // Stampo per la classe figlia cucce
-echo "<br>";
-$kennel1 = new KennelProduct("Cuccia", "80$", $dog, "3mq", "Cotton");
-echo $kennel1->GetInfo(); 
+//echo "<br>";
+//$kennel1 = new KennelProduct("Cuccia", "80$", $dog, "3mq", "Cotton");
+//echo $kennel1->GetInfo(); 
 
 ?>
 
@@ -104,9 +112,13 @@ echo $kennel1->GetInfo();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Animal Kingdom Cards</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
 </body>
 </html>
